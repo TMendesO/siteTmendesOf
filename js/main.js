2c8eb0170ccdbbc78d19cdabe3250f618d667bc1
Thiago Mendes <37598914+ThiagoMendes1@users.noreply.github.com>
2020-05-18 18:08:06 -0300

    modal-funcionou
    o modal funcionou, falta a corrigir funcionamento

--- js/main.js
+++ js/main.js
@@ -1,10 +1,3 @@
-
-$(document).ready(function(){
-    $(".navbar-toggle").click(function(){
-        $(".menu").toggleClass("menu-open");
-    })
-});
-
-      $('#myModal').on('shown.bs.modal', function () {
+ $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').trigger('focus')
 });
