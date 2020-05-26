d63c3c188ea70f8cae6137e9334698fab01655f3
Thiago Mendes <37598914+ThiagoMendes1@users.noreply.github.com>
2020-05-26 18:38:27 -0300

    coloquei T-rex

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+$('.btn').on('click', function(e) {
+    e.preventDefault();
+    var url = $(this).attr('href');
+    $(".modal-body").html('<iframe width="100%" height="100%" frameborder="0" scrolling="yes" allowtransparency="true" src="'+url+'"></iframe>');
+});
 /*onclick = function () {
     $('body').css('background-color', 'red');
 }
