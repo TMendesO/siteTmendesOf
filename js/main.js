b7289a2b4279aea8e106a1dc46d88847f7b31f94
Thiago Mendes <37598914+ThiagoMendes1@users.noreply.github.com>
2020-05-27 18:58:08 -0300

    corrigi posicionamento
    corri o posicionamento e estou preparando para implantar paralax

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
-$('.btn').on('click', function(e) {
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+});
+
+
+$('.btn').on('click', function (e) {
     e.preventDefault();
     var url = $(this).attr('href');
-    $(".modal-body").html('<iframe width="100%" height="100%" frameborder="0" scrolling="yes" allowtransparency="true" src="'+url+'"></iframe>');
+    $(".modal-body").html('<iframe width="100%" height="100%" frameborder="0" scrolling="no" allowtransparency="true" src="' + url + '"></iframe>');
 });
 /*onclick = function () {
     $('body').css('background-color', 'red');
