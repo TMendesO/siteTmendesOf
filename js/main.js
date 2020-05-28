b3368bbbd07f1b811b3b12a46315d6901597cdcb
Thiago Mendes <37598914+ThiagoMendes1@users.noreply.github.com>
2020-05-28 19:18:49 -0300

    Montando o Parallax
    coloquei as imagens em posição, falta acertar efeito e animação

--- js/main.js
+++ js/main.js
@@ -8,13 +8,3 @@ $('.btn').on('click', function (e) {
     var url = $(this).attr('href');
     $(".modal-body").html('<iframe width="100%" height="100%" frameborder="0" scrolling="no" allowtransparency="true" src="' + url + '"></iframe>');
 });
-/*onclick = function () {
-    $('body').css('background-color', 'red');
-}
-
-
-
-$('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-});
-*/
