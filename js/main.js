16cb7ed16efc4232583743a616dab9d984630a9a
Thiago Mendes <37598914+ThiagoMendes1@users.noreply.github.com>
2020-05-29 19:26:24 -0300

    Se de ruim-pegar esse
    tive problemas, recuperei com problemas, estou colocando novamente no llugar.

--- js/main.js
+++ js/main.js
@@ -1,28 +1,14 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 $('#dino').on('shown.bs.modal', function () {
-=======
-$('#myModal').on('shown.bs.modal', function () {
->>>>>>> parent of 6e7d721... Erro-ficou-ruim
+
+
     $('#myInput').trigger('focus')
 });
 
 
-$('.btn').on('click', function (e) {
-=======
 $('.btn').on('click', function(e) {
->>>>>>> parent of b7289a2... corrigi posicionamento
+
     e.preventDefault();
     var url = $(this).attr('href');
     $(".modal-body").html('<iframe width="100%" height="100%" frameborder="0" scrolling="yes" allowtransparency="true" src="'+url+'"></iframe>');
 });
-/*onclick = function () {
-    $('body').css('background-color', 'red');
-}
-
-
-
-$('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-});
-*/
