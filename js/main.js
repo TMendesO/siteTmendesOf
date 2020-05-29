249bdbaea53a3898aaaac7442645dbe61de5723c
Thiago Mendes <37598914+ThiagoMendes1@users.noreply.github.com>
2020-05-29 17:26:38 -0300

    voltei arquivos de ontem
    deu erro

--- js/main.js
+++ js/main.js
@@ -1,10 +1,14 @@
+<<<<<<< HEAD
 $('#dino').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
 });
 
 
 $('.btn').on('click', function (e) {
+=======
+$('.btn').on('click', function(e) {
+>>>>>>> parent of b7289a2... corrigi posicionamento
     e.preventDefault();
     var url = $(this).attr('href');
-    $(".modal-body").html('<iframe width="100%" height="100%" frameborder="0" scrolling="no" allowtransparency="true" src="' + url + '"></iframe>');
+    $(".modal-body").html('<iframe width="100%" height="100%" frameborder="0" scrolling="yes" allowtransparency="true" src="'+url+'"></iframe>');
 });
