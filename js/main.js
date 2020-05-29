f7a390ed314e6682269477d8edbea3ed6afa0b97
Thiago Mendes <37598914+ThiagoMendes1@users.noreply.github.com>
2020-05-29 17:34:59 -0300

    voltei arquivos de ontem2
    deu erro

--- js/main.js
+++ js/main.js
@@ -12,3 +12,13 @@ $('.btn').on('click', function(e) {
     var url = $(this).attr('href');
     $(".modal-body").html('<iframe width="100%" height="100%" frameborder="0" scrolling="yes" allowtransparency="true" src="'+url+'"></iframe>');
 });
+/*onclick = function () {
+    $('body').css('background-color', 'red');
+}
+
+
+
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+});
+*/
