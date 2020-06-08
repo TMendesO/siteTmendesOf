7d4b609d3a5270b20fb8698bc04cd2384eda4be4
Thiago Mendes <37598914+ThiagoMendes1@users.noreply.github.com>
2020-06-08 20:41:24 -0300

    teste de efeito display none

--- js/main.js
+++ js/main.js
@@ -12,16 +12,6 @@ $('.btn').on('click', function (e) {
     $(".modal-body").html('<iframe width="100%" height="100%" frameborder="0" scrolling="yes" allowtransparency="true" src="' + url + '"></iframe>');
 });
 
-
-$("#btnTeste").click(function () {
-    //Sem parâmetros: o efeito é executado em 400ms
-    $("#minhaDiv").slideUp();
-    //Parâmetro com a duração em ms do efeito
-    $("#minhaDiv").slideUp(1000);
-    //Informada duração e função de callback
-    $("#minhaDiv").slideUp("fast",
-        function () {
-            alert("Slide concluido");
-        }
-    );
+$("#btn1").click(function () {
+    $("#minhaDiv").css("display", "block");
 });
