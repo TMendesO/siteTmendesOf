3ab901b91777f3847ccfdefb76d2616e8bd6656c
Thiago Mendes <37598914+ThiagoMendes1@users.noreply.github.com>
2020-06-08 20:21:36 -0300

    alterado o fundo-08-06-2020
    testando efeitos

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 $('#dino').on('shown.bs.modal', function () {
 
 
@@ -6,9 +5,23 @@ $('#dino').on('shown.bs.modal', function () {
 });
 
 
-$('.btn').on('click', function(e) {
+$('.btn').on('click', function (e) {
 
     e.preventDefault();
     var url = $(this).attr('href');
-    $(".modal-body").html('<iframe width="100%" height="100%" frameborder="0" scrolling="yes" allowtransparency="true" src="'+url+'"></iframe>');
+    $(".modal-body").html('<iframe width="100%" height="100%" frameborder="0" scrolling="yes" allowtransparency="true" src="' + url + '"></iframe>');
+});
+
+
+$("#btnTeste").click(function () {
+    //Sem parâmetros: o efeito é executado em 400ms
+    $("#minhaDiv").slideUp();
+    //Parâmetro com a duração em ms do efeito
+    $("#minhaDiv").slideUp(1000);
+    //Informada duração e função de callback
+    $("#minhaDiv").slideUp("fast",
+        function () {
+            alert("Slide concluido");
+        }
+    );
 });
