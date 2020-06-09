90b1ad8ae73bf04ff80565449b1a857cac97bef0
Thiago Mendes <37598914+ThiagoMendes1@users.noreply.github.com>
2020-06-09 18:34:27 -0300

    criei animação com scroll-09-06-2020
    criei uma animação, falta acertar e colocar a função do botão.

--- js/main.js
+++ js/main.js
@@ -12,6 +12,59 @@ $('.btn').on('click', function (e) {
     $(".modal-body").html('<iframe width="100%" height="100%" frameborder="0" scrolling="yes" allowtransparency="true" src="' + url + '"></iframe>');
 });
 
-$("#btn1").click(function () {
-    $("#minhaDiv").css("display", "block");
+debounce = function (func, wait, immediate) {
+    var timeout;
+    return function () {
+        var context = this,
+            args = arguments;
+        var later = function () {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+};
+
+
+
+
+
+
+
+
+(function () {
+    var $target = $('.content'),
+        animationClass = 'content-start',
+        offset = $(window).height() * 1.4;
+
+    function animeScroll() {
+        var documentTop = $(document).scrollTop();
+
+        $target.each(function () {
+            var itemTop = $(this).offset().top;
+            if (documentTop > itemTop - 30) {
+                $(this).addClass(animationClass);
+            } else {
+                $(this).removeClass(animationClass);
+            }
+        })
+    }
+
+    animeScroll();
+    $(document).scroll(debounce(function () {
+        animeScroll();
+    }, 160));
+}());
+
+/*$('#btn1').click(function () {
+    animeScroll();
 });
+
+$(function () {
+    $('#btn1').click(function () {
+        $(this).siblings('div#bio').slideToggle();
+    })
+});*/
